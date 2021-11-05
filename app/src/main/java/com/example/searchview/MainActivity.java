740bbf2db12df2b5070bd3fef809847110945833
otienotilen <otienotilen@gmail.com>
2021-11-05 12:48:55 +0300

    Search view code

---
+++ app/src/main/java/com/example/searchview/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.searchview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    String[] name = {"Tilen","Fidel", "Glen","Larissa","Tausi","Fred","Ochieng","Abonyo"};
+    ArrayAdapter<String> arrayAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listview);
+        arrayAdapter= new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,name);
+        listView.setAdapter(arrayAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setQueryHint("Type here to search");
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                arrayAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+}
